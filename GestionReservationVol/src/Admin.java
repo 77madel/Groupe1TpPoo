096960bb8b61ec4266ceaf50964820cc2b5cd389
DouyonDev <abdoulayedouyon.ad@gmail.com>
2024-05-10 16:02:41 +0000

    envoi des trois classes dans le dossier du groupe de ma branche

---
+++ GestionReservationVol/src/Admin.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class Admin extends Personne {
+    private int idAdmin;
+
+    // Constructeur
+    public Admin(int idPersonne, String nom, String prenom, String email, String numeroDeTelephone, Date dateDeNaissance, String motDePasse, int idAdmin) {
+        super(idPersonne, nom, prenom, email, numeroDeTelephone, dateDeNaissance, motDePasse);
+        this.idAdmin = idAdmin;
+    }
+
+    // Getter et Setter spécifique à Admin
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public void ajouterCompagnie(CompagnieAerienne compagnie) throws SQLException {
+        String ajoutComp = "INSERT INTO Compagnie (nomCompagnie, nombreDAvion, motDePasse, siteWeb, idAdmin) VALUES (?, ?, ?, ?, ?)";
+        try (Connection connection = ConnectDB.getConnection();
+            PreparedStatement statement = connection.prepareStatement(ajoutComp)) {
+            statement.setString(1, compagnie.getNom_comp());
+            statement.setInt(2, compagnie.getNbred_avion());
+            statement.setString(3, compagnie.getMdp());
+            statement.setString(4, compagnie.getSiteWeb());
+            statement.setInt(5, this.getIdAdmin()); // Utilise l'ID de l'admin courant
+            statement.executeUpdate();
+        }
+    }
+
+    public void supprimerCompagnie(int idCompagnie) throws SQLException {
+        String supprComp = "DELETE FROM Compagnie WHERE idCompagnie = ?";
+        try (Connection connection = ConnectDB.getConnection();
+            PreparedStatement statement = connection.prepareStatement(supprComp)) {
+            statement.setInt(1, idCompagnie);
+            statement.executeUpdate();
+        }
+    }
+
+    public void modifierCompagnie(CompagnieAerienne compagnie) throws SQLException {
+        String modifComp = "UPDATE Compagnie SET nomCompagnie = ?, nombreDAvion = ?, motDePasse = ?, siteWeb = ? WHERE idCompagnie = ?";
+        try (Connection connection = ConnectDB.getConnection();
+            PreparedStatement statement = connection.prepareStatement(modifComp)) {
+            statement.setString(1, compagnie.getNom_comp());
+            statement.setInt(2, compagnie.getNbred_avion());
+            statement.setString(3, compagnie.getMdp());
+            statement.setString(4, compagnie.getSiteWeb());
+            statement.setInt(5, compagnie.getId_comp());
+            statement.executeUpdate();
+        }
+    }
+}
--- main.java
+++ main.java
@@ -1,21 +1,15 @@
-import java.sql.SQLException;
 
-public class main {
+
+public class Main {
     public static void main(String[] args) {
-        // Création d'une instance de la classe Admin
-        Admin admin = new Admin(1, "GOITA", "Assimi", "assimigoita@gmail.com", "Mali", 1);
-        
-        // Utilisation des méthodes de l'objet Admin
-        String nom = admin.getNom(); // Utilisation de la méthode getNom() héritée de la classe Personne
-        System.out.println("Nom de l'admin : " + nom); // Affichage du nom de l'admin
-        
-        
-        CompagnieAerienne compagnie = new CompagnieAerienne(1, "Air Mali", 0, "123", null, 0);
-        try {
-            admin.ajouterCompagnie(compagnie);
-            System.out.println("Compagnie aérienne ajoutée avec succès.");
-        } catch (SQLException e) {
-            System.err.println("Erreur lors de l'ajout de la compagnie aérienne : " + e.getMessage());
-        }
+        // Création d'une instance de la classe Admin avec des valeurs factices
+        Admin admin = new Admin(1, "GOITA", "Assimi", "assimigoita@gmail.com", null, null, null, 0);
+
+        // Utilisation de la méthode getNom() pour récupérer le nom de l'admin
+        String nom = admin.getNom();
+
+        // Affichage du nom de l'admin
+        System.out.println("Nom de l'admin : " + nom);
     }
 }
+
